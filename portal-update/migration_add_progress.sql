f5669c1d47c49dfa5cfdcd660a15de7e39bea091
ffwebsystems <ffwebsystems@gmail.com>
2026-08-07 20:10:22 -0400

    Delete portal-update directory

--- portal-update/migration_add_progress.sql
+++
@@ -1,4 +0,0 @@
--- Run this only if you already applied the original schema.sql before
--- progress tracking was added. Safe to skip if deploying schema.sql fresh.
-
-ALTER TABLE clients ADD COLUMN progress_stage INTEGER NOT NULL DEFAULT 0;
